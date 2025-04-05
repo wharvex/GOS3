13c71545e9e52aa299d8e03e60af2ee9581d670e
Tim <81175852+wharvex@users.noreply.github.com>
2025-04-05 13:53:57 -0400

    circular dependency error

--- app/src/main/java/com/wharvex/gos/injector/AppInjector.java
+++ app/src/main/java/com/wharvex/gos/injector/AppInjector.java
@@ -2,6 +2,9 @@ package com.wharvex.gos.injector;
 
 import com.google.inject.AbstractModule;
 import com.google.inject.assistedinject.FactoryModuleBuilder;
+import com.wharvex.gos.actionlistener.AbstractActionListener;
+import com.wharvex.gos.actionlistener.IActionListenerFactory;
+import com.wharvex.gos.actionlistener.ProcessButtonActionListener;
 import com.wharvex.gos.logger.ILogger;
 import com.wharvex.gos.logger.TestLogger;
 import com.wharvex.gos.process.AbstractKernelProcess;
@@ -21,6 +24,10 @@ public class AppInjector extends AbstractModule {
     bind(AbstractKernelProcess.class).to(KernelProcess.class);
     install(new FactoryModuleBuilder().implement(AbstractSemaphore.class,
         OwnedBinarySemaphore.class).build(ISemaphoreFactory.class));
+//    install(
+//        new FactoryModuleBuilder().implement(AbstractActionListener.class,
+//                ProcessButtonActionListener.class)
+//            .build(IActionListenerFactory.class));
 
     // Uncomment to switch to MessageServiceB
     // bind(IMessageService.class).to(MessageServiceB.class);
