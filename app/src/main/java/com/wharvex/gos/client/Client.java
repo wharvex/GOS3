872f1467b517bf949b7c12a4caf9e23654aaed81
Tim <81175852+wharvex@users.noreply.github.com>
2025-04-05 12:52:04 -0400

    new approach to action listener package

--- app/src/main/java/com/wharvex/gos/client/Client.java
+++ app/src/main/java/com/wharvex/gos/client/Client.java
@@ -1,14 +1,11 @@
 package com.wharvex.gos.client;
 
 import com.google.inject.Inject;
-import com.wharvex.gos.logger.ILogger;
 import com.wharvex.gos.window.AbstractWindow;
 
 import javax.swing.*;
 
 public class Client {
-  @Inject
-  private ILogger logger;
   @Inject
   private AbstractWindow window;
 
