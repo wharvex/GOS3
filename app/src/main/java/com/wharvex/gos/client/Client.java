09bc1e161788fbb771407f706004a029219ffe0e
Tim <81175852+wharvex@users.noreply.github.com>
2025-04-05 14:09:16 -0400

    no longer broken
    Removing the ILogger dependency from Client causes a circular
    dependency, for some reason.

--- app/src/main/java/com/wharvex/gos/client/Client.java
+++ app/src/main/java/com/wharvex/gos/client/Client.java
@@ -2,10 +2,13 @@ package com.wharvex.gos.client;
 
 import com.google.inject.Inject;
 import com.wharvex.gos.window.AbstractWindow;
+import com.wharvex.gos.logger.ILogger;
 
 import javax.swing.*;
 
 public class Client {
+  @Inject
+  private ILogger logger;
   @Inject
   private AbstractWindow window;
 
